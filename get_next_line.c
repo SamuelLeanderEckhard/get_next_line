4c2947138dd9c2bdfbe71b7094ad0b56c54a30ad
Samuel Leander Eckhard <131253874+SamuelLeanderEckhard@users.noreply.github.com>
2023-10-23 22:29:13 +0200

    Add files via upload

--- get_next_line.c
+++ get_next_line.c
@@ -6,9 +6,63 @@
 /*   By: seckhard <seckhard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/06 20:22:53 by seckhard          #+#    #+#             */
-/*   Updated: 2023/10/10 16:20:06 by seckhard         ###   ########.fr       */
+/*   Updated: 2023/10/23 21:11:06 by seckhard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+static int	read_line(int fd, char **line, char **remainder)
+{
+	char		buffer[BUFFER_SIZE + 1];
+	int			bytes_read;
+	char		*tmp;
+	static char	*remaining_data = NULL;
+
+	bytes_read = read(fd, buffer, BUFFER_SIZE)
+
+	if (fd < 0 || !line)
+		return (-1);
+	if (!remaining_data)
+		remaining_data = ft_strdup("");
+
+	while (bytes_read > 0)
+	{
+		buffer[bytes_read] = '\0';
+		tmp = ft_strjoin(remaining_data, buffer);
+		free(remaining_data);
+		remaining_data = tmp;
+		if (ft_strchr(buffer, '\n'))
+			return (find_newline(line));
+	}
+	if (bytes_read == 0 && remaining_data[0] != '\0')
+		return (find_newline(line));
+	free(remaining_data);
+	remaining_data = NULL;
+	return (0);
+}
+
+int	find_newline(char **line)
+{
+	char	*newline_pos;
+	char	*tmp;
+
+	newline_pos = ft_strchr(remaining_data, '\n');
+	if (newline_pos != NULL)
+	{
+		*newline_pos = '\0';
+		*line = ft_strdup(remaining_data);
+		*tmp = ft_strdup(newline_pos + 1);
+		free(remaining_data);
+		remaining_data = tmp;
+		return (1);
+	}
+	return (0);
+}
+
+int	get_next_line(int fd, char **line)
+{
+	if (read_line(fd, line) == 1)
+		return (1);
+	return (0);
+}
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: seckhard <seckhard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/06 20:23:07 by seckhard          #+#    #+#             */
-/*   Updated: 2023/10/10 16:18:17 by seckhard         ###   ########.fr       */
+/*   Updated: 2023/10/14 21:27:58 by seckhard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,3 +15,11 @@
 
 # include <stdlib.h>
 # include <unistd.h>
+
+int		ft_strlen(const char *str);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strchr(const char *str, int character);
+char	*ft_strdup(const char *s);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+
+#endif
